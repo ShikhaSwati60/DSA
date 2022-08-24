268a888954f22ae1eb9d1449c998177f97cb86a3
Swati Shikha <shikhaswati7670@gmail.com>
2022-08-24 23:26:03 +0530

    Middle element of a linked list

---
+++ src/Linked_List/MiddleNode.java
@@ -0,0 +1,31 @@
+package Linked_List;
+
+import java.util.Scanner;
+
+public class MiddleNode extends Linked_List{
+    static int midNode(Node head)
+    {
+        Node slow=head;
+        Node fast=head;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow.data;
+    }
+    public static void main(String[] args) {
+            MiddleNode list=new MiddleNode();
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no. of nodes in the list: ");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            list.insert(sc.nextInt());
+        }
+        System.out.print("Linked list: ");
+        list.display();
+        int middle= midNode(list.head);
+        System.out.println("\nMiddle element of the list is: "+middle);
+    }
+}
